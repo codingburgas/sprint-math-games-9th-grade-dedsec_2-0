a82a68e10ccfd55410d459595d66286399774649
Milen Vasilev <MIVasilev23@codingburgas.bg>
2024-11-25 00:57:47 +0200

    Fix some bugs in the gameMechanics (Close #13)

--- keyW0rd/keyW0rd/gameMechanics.cpp
+++ keyW0rd/keyW0rd/gameMechanics.cpp
@@ -5,8 +5,10 @@
 #include <string>
 
 using namespace std;
-void gameMechanics() {
-    cout << "\nYou have 12 attempts to guess the 4 words!" << endl;
+
+void gameMechanics()
+{
+    cout << "\n You have 12 attempts to guess the 4 words!" << endl;
 
     string secretWords[4];
     randomWords(secretWords);
@@ -14,27 +16,39 @@ void gameMechanics() {
     int attempts = 12;
     string guess;
 
+    bool isCorrect[4] = { false, false, false, false };
 
     while (attempts > 0) {
-        cout << "\nAttempts left: " << attempts << endl;
+        cout << "\n Attempts left: " << attempts << endl;
         cout << "Enter your guess (5 letters): ";
         cin >> guess;
 
         if (guess.length() != 5) {
-            cout << "\n[!] Please enter a 5-letter word! [!]" << endl;
+            cout << "\n [!] Please enter a 5-letter word! [!]" << endl;
             continue;
         }
 
         bool allCorrect = true;
 
-        for (int i = 0; i < 4; i++) {
-            cout << "\nWord " << i + 1 << ":\n";
+        for (int i = 0; i < 4; i++)
+        {
+            cout << "\n Word " << i + 1 << ":\n";
 
-            if (guess == secretWords[i]) {
+            if (isCorrect[i])
+            {
+                cout << "+---+---+---+---+---+" << endl;
+                cout << "| " << secretWords[i][0] << " | " << secretWords[i][1] << " | " << secretWords[i][2] << " | " << secretWords[i][3] << " | " << secretWords[i][4] << " |" << endl;
+                cout << "+---+---+---+---+---+" << endl;
+                cout << "Correct!" << endl;
+            }
+            else if (guess == secretWords[i])
+            {
                 cout << "+---+---+---+---+---+" << endl;
                 cout << "| " << guess[0] << " | " << guess[1] << " | " << guess[2] << " | " << guess[3] << " | " << guess[4] << " |" << endl;
                 cout << "+---+---+---+---+---+" << endl;
                 cout << "Correct!" << endl;
+
+                isCorrect[i] = true;
             }
             else {
                 cout << "+---+---+---+---+---+" << endl;
@@ -55,16 +69,20 @@ void gameMechanics() {
         }
 
         if (allCorrect) {
+            cout << "\n==============================================" << endl;
             cout << "\nCongratulations! You've guessed all the words!" << endl;
+            cout << "\n==============================================" << endl;
             return;
         }
         attempts--;
     }
-
+    cout << "\n================================" << endl;
     cout << "\nOut of attempts! The words were:" << endl;
+    cout << "\n================================" << endl;
+    cout << " " << endl;
     for (int i = 0; i < 4; i++) {
         cout << "Word " << i + 1 << ": " << secretWords[i] << endl;
     }
+
     cout << "Better luck next time!" << endl;
-    
 }
